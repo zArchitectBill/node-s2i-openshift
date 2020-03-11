12fa162d4879a07669180830b1eaf38e8ce64649
teamNN-wsc <61852315+teamNN-wsc@users.noreply.github.com>
2020-03-11 13:12:54 -0400

    Update app.js

--- site/app.js
+++ site/app.js
@@ -52,12 +52,12 @@ app.get('/info', function(req, res) {
   if (CURRENTMODE == MODE.TEST) {
     patientdata = {
       "personal": {
-        "name": "Ralph DAlmeida",
-        "age": 38,
+        "name": "Matt Mondics",
+        "age": 27,
         "gender": "male",
-        "street": "34 Main Street",
-        "city": "Toronto",
-        "zipcode": "M5H 1T1"
+        "street": "1234 State Street",
+        "city": "Cleveland",
+        "zipcode": "44106"
       },
       "medications": ["Metoprolol", "ACE inhibitors", "Vitamin D"],
       "appointments": ["2018-01-15 1:00 - Dentist", "2018-02-14 4:00 - Internal Medicine", "2018-09-30 8:00 - Pediatry"]
