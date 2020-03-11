a3aff8fa61a919b574057c195011877f6d795d8e
teamNN-wsc <61852315+teamNN-wsc@users.noreply.github.com>
2020-03-11 13:31:50 -0400

    Update app.js

--- site/app.js
+++ site/app.js
@@ -52,7 +52,7 @@ app.get('/info', function(req, res) {
   if (CURRENTMODE == MODE.TEST) {
     patientdata = {
       "personal": {
-        "name": "Ralph DAlmeida",
+        "name": "Matt DAlmeida",
         "age": 38,
         "gender": "male",
         "street": "34 Main Street",
